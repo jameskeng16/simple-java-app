de73ca0ce6e7ef0e9cb71d1eca42e571551a5c87
jameskeng16 <jameskeng16@gmail.com>
2025-04-09 16:26:12 +0800

    simulating an error

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modified File!");
+        System.out.println("Jenkins is da best!");
     }
 
     public String getMessage() {
