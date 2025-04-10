bc3caec4a78eacb2fcdcdced5280743220b9fc1f
James Keng <jameskeng16@gmail.com>
2025-04-10 14:57:52 +0800

    git hook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I dont like jenkins");
+        System.out.println("I HATE jenkins");
     }
 
     public String getMessage() {
